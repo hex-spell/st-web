7a012ae9bf7b33a7fa0fa418ccdecf9a7c6e24b6
Vagus-art <agustin.favo@gmail.com>
2020-01-24 17:35:58 -0300

    animations are friendlier

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,8 @@ const controller = new ScrollMagic.Controller();
 
 $(".material-showcase").each(function(index) {
   const tl = new gsap.timeline();
-  const direction = Math.pow(-1, index) * 200;
-  tl.from($(this), { x: direction, opacity: 0.5 });
+  const direction = Math.pow(-1, index) * 25;
+  tl.from($(this), 1, { x: direction, opacity: 0 });
   new ScrollMagic.Scene({
     triggerElement: this
   })
