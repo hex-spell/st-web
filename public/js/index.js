bc34f5afd2a3e706bb5da62b4af77ca5828d40f1
Vagus-art <agustin.favo@gmail.com>
2020-01-24 17:29:48 -0300

    prettier

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,12 @@
 const controller = new ScrollMagic.Controller();
 
-$('.material-showcase').each(function(index){
-    const tl = new gsap.timeline(); 
-    const direction = Math.pow(-1,index)*200;
-    tl.from($(this),{x:direction,opacity:0.5})
-    new ScrollMagic.Scene({
-        triggerElement:this
-    })
-        .setTween(tl)
-        .addTo(controller);
+$(".material-showcase").each(function(index) {
+  const tl = new gsap.timeline();
+  const direction = Math.pow(-1, index) * 200;
+  tl.from($(this), { x: direction, opacity: 0.5 });
+  new ScrollMagic.Scene({
+    triggerElement: this
+  })
+    .setTween(tl)
+    .addTo(controller);
 });
-
